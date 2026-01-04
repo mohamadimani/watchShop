4b1074d5951abc26c7e44d1fa6e2cb799986f2ed
manimohamadi <mohamadimahdi@yahoo.com>
2026-01-05 00:04:03 +0330

    ++

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,15 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Basket;
+use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
-    public function index() {
+    public function index() {}
 
-    }
-    
-    public function store() {
+    public function store()
+    {
+        $baskets = auth()->user()->basket;
+        if ($baskets->count() > 0) {
+            $totalAmount = $baskets->sum(function ($basket) {
+                $price = $basket->product->price - ($basket->product->price * $basket->product->discount) / 100;
+                $totalPrice = $price * $basket->quantity;
+                return $totalPrice;
+            });
+            DB::beginTransaction();
+            try {
+                $order = Order::create([
+                    'user_id' => user()->id,
+                    'total_amount' => $totalAmount,
+                    'created_by' => user()->id,
+                    'status' => 'pending',
+                ]);
 
+                $order->orderProducts()->createMany($baskets->toArray());
+                Basket::query()->where('user_id', user()->id)->delete();
+                DB::commit();
+                return true;
+            } catch (\Exception $e) {
+                DB::rollBack();
+                throw $e;
+            }
+        }
+        return false;
     }
 }
